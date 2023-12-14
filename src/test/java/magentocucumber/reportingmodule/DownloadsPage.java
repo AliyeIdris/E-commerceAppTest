2ab8709e17cc87025a690dc71194ff675b651e7f
salman <salmanuyghur3@gmail.com>
2023-12-14 23:28:59 +0300

    report_Module

---
+++ src/test/java/magentocucumber/reportingmodule/DownloadsPage.java
@@ -0,0 +1,10 @@
+package magentocucumber.reportingmodule;
+
+/**
+ * @author : SalmanUyghur
+ * @created : 12/13/2023,12:01 PM
+ * @Email : salmanuyghur3@gmail.com
+ **/
+public class DownloadsPage {
+
+}
