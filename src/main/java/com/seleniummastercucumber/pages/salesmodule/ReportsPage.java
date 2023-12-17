18b40445a5dd1220daa998c12402fb24d11f9a9f
xamxinur <135423963+xamxinur@users.noreply.github.com>
2023-12-17 22:54:37 +0100

    cucumber-01

---
+++ src/main/java/com/seleniummastercucumber/pages/salesmodule/ReportsPage.java
@@ -0,0 +1,11 @@
+package com.seleniummastercucumber.pages.salesmodule;
+
+import com.seleniummastercucumber.utility.FunctionLibrary;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ReportsPage {
+    WebDriver driver;
+    FunctionLibrary functionLibrary;
+    Actions actions;
+}
